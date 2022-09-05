c28d0e71dfbfc6c1c1339562031bde075bad0797
ryhhill1998 <ryhhill1998@outlook.com>
2022-09-05 08:54:29 +0100

    Added categories to homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import "./App.css";
+import Directory from "./components/directory/directory.component";
 
-function App() {
-  return <div className="App">Hello world!</div>;
-}
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "About us",
+      imageSrc: `${process.env.PUBLIC_URL}/images/photo.png`,
+    },
+    {
+      id: 2,
+      title: "Reviews",
+      imageSrc: `${process.env.PUBLIC_URL}/images/photo.png`,
+    },
+    {
+      id: 3,
+      title: "Recipes",
+      imageSrc: `${process.env.PUBLIC_URL}/images/photo.png`,
+    },
+    {
+      id: 4,
+      title: "Lifestyle",
+      imageSrc: `${process.env.PUBLIC_URL}/images/photo.png`,
+    },
+    {
+      id: 5,
+      title: "Contact us",
+      imageSrc: `${process.env.PUBLIC_URL}/images/photo.png`,
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
 
 export default App;
